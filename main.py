03daaac911ee341907e8d75d975d0a253ae119ef
contaproxy100 <contaale62@gmail.com>
2025-06-25 15:28:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import sys
 from proxy import main
 
 if __name__ == '__main__':
-    main([
+    sys.argv = [
+        'proxy.py',
         '--hostname', '0.0.0.0',
         '--port', '3128',
         '--upstream-proxy', 'http://dolar2020:dolar2020@154.91.152.38:7777'
-    ])
+    ]
+    main()
